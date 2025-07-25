2074067a2110f0d94bbce2ef3532c245840d1fda
Beemil <valeskavr17@gmail.com>
2025-07-24 21:42:48 -0600

    Create Product Class

---
+++ Pedidos/Producto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedidos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("***** DATOS DEL PRODUCTO *****");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Precio: {Precio}");
+            Console.WriteLine($"Cantidad disponible: {Stock}");
+            Console.WriteLine();
+        }
+
+        //Si el constructor no lleva parametros, C# automaticamente lo crea por detras
+
+       
+
+        public void DisminuirStock(int Cantidad)
+        {
+            if (Stock >= Cantidad)
+            {
+                Stock -= Cantidad;
+            }
+            else 
+            {
+                Console.WriteLine($"No hay suficiente stock de {Nombre}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Pedidos/Program.cs
+++ Pedidos/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Pedidos;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Producto producto = new Producto();
+producto.Id = 1;
+producto.Nombre = "Laptop";
+producto.Precio = 5000;
+producto.Stock = 10;
+
+producto.MostrarInformacion();
+producto.DisminuirStock(2);
+producto.MostrarInformacion();
+
+producto.DisminuirStock(15);
+producto.MostrarInformacion();
+
+Producto producto1 = new Producto();
+producto.Id = 2;
+producto.Nombre = "Mouse";
+producto.Precio = 250;
+producto.Stock = 15;
+
+producto.MostrarInformacion();
