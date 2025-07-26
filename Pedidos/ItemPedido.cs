72bda941b94cddf4ee9414977b34f220daef55f7
Beemil <valeskavr17@gmail.com>
2025-07-25 19:44:01 -0600

    Create ItemPedido

---
+++ Pedidos/ItemPedido.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pedidos
+{
+    internal class ItemPedido
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal => Producto.Precio * Cantidad;
+
+        public ItemPedido(Producto Producto, int Cantidad) 
+        { 
+            this.Producto = Producto;
+            this.Cantidad = Cantidad;
+        }
+
+    }
+}
